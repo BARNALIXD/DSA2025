06316f2f6a9622edbba6e727df7374dbc78c9532
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-03-12 08:21:50 +0530

    c2

--- Countvowels.java
+++ Countvowels.java
@@ -1,3 +1,12 @@
 public class Countvowels {
-    
+    public static  int vowels(String str) {
+        int count = 0;
+        str = str.toLowerCase();
+        for(char ch: str.toCharArray()) {
+            if ("aeiou".indexOf(ch) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
